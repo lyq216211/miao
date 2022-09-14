47b0716a3627ffac39b32cf709301073c1db9798
lyq <1322677782@qq.com>
2022-09-14 22:19:09 +0800

    lodash

--- lodash/lyq216211-lodash.js
+++ lodash/lyq216211-lodash.js
@@ -37,4 +37,68 @@ var lyq216211 = {
     }
     return result
   },
+
+  drop: function (arr, number = 1) {
+    return arr.slice(number)
+  },
+
+  fill: function (array, value, start = 0, end = array.length) {
+    for (let i = start; i < end; i++) {
+      array[i] = value
+    }
+  },
+
+  findIndex: function (array, f, fromIndex = 0) {
+    for (let i = fromIndex; i < array.length; i++) {
+      let item = array[i]
+      if (f(item)) {
+        return i
+      }
+    }
+  },
+
+  findLastIndex: function () {
+
+  },
+  flatten: function (array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        for (let j = 0; j < array[i].length; j++) {
+          result.push(array[i][j])
+        }
+      } else {
+        result.push(array[i])
+      }
+    }
+
+    return result
+  },
+
+  flattenDeep: function (array) {
+    return array.reduce((result, item) => {
+      return Array.isArray(item)
+        ? result.concat(flatten(item))
+        : result.concat(item);
+    }, []);
+  },
+
+  flattenDepth: function (array, depth = 1) {
+    if (depth === 0) {
+      return array
+    }
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        let flat = flattenDepth(array[i], depth - 1)
+        for (let j = 0; j < flat.length; j++) {
+          result.push(flat[j])
+        }
+      } else {
+        result.push(array[i])
+      }
+    }
+
+    return result
+  },
 }
