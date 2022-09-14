8a1bc20934941682f6d9499470c4cd8a2d1c4668
lyq <1322677782@qq.com>
2022-09-14 17:56:13 +0800

    lodash

---
+++ lodash/lyq216211-lodash.js
@@ -0,0 +1,19 @@
+var lyq216211 = {
+  chunk: function (array, number) {
+    let result = []
+    let start = 0
+    let end = start + number
+
+    while (start < array.length) {
+      if (end < array.length) {
+        result.push(array.slice(start, end))
+      } else {
+        result.push(array.slice(start))
+      }
+      start += number
+      end += number
+    }
+
+    return result
+  }
+}
