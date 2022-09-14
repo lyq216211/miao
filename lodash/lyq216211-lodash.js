19227e4b4c83d60435fda0ca8054657adbbe9a1b
lyq <1322677782@qq.com>
2022-09-14 20:40:16 +0800

    lodash

--- lodash/lyq216211-lodash.js
+++ lodash/lyq216211-lodash.js
@@ -1,5 +1,5 @@
 var lyq216211 = {
-  chunk: function (array, number) {
+  chunk: function (array, number = 1) {
     let result = []
     let start = 0
     let end = start + number
@@ -15,5 +15,26 @@ var lyq216211 = {
     }
 
     return result
-  }
+  },
+
+  compact: function (arr) {
+    let result = []
+    for (let item of arr) {
+      if (item) {
+        result.push(item)
+      }
+    }
+    return result
+  },
+
+  difference: function (arr, ...values) {
+    let result = []
+    values = values.flat()
+    for (let i = 0; i < arr.length; i++) {
+      if (!(values.includes(arr[i]))) {
+        result.push(arr[i])
+      }
+    }
+    return result
+  },
 }
